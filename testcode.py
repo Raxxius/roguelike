1592fad2909ce2998ef4f29fb1c1d5f6a3fcb0b0
Raxxius <Raxxman@gmail.com>
2021-11-24 16:43:32 +0000

    updating code for creating new sheets in google

---
+++ testcode.py
@@ -0,0 +1,19 @@
+import random
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('roguelike')
+
+
+SHEET.add_worksheet(title="A_worksheet", rows="40", cols="20")
+set_row_height("A_worksheet", 5, 5)
