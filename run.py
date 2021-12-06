b30d28f4d92e050a0009972f61242b56e5346b9c
Raxxius <Raxxman@gmail.com>
2021-12-06 14:42:01 +0000

    corrected another error in test text

--- run.py
+++ run.py
@@ -240,7 +240,7 @@ def gamescreen(stdscr, character):
     # test code
     for y in range(0, 40):
         for x in range(0, 20):
-            pad.addch(y,x, ord('a') + (x*x+y*y) % 26)
+            map.addch(y,x, ord('a') + (x*x+y*y) % 26)
     try:
         map.refresh(0, 0, 0, 26, 23, 79)
     except curses.error:
